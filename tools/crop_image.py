a34dc0ed386fb79576b25c660a84505c1eec6ea8
Yanqing Wu <14853314+pwyq@users.noreply.github.com>
2020-10-29 21:07:08 -0400

    resize all ship images

--- tools/crop_image.py
+++ tools/crop_image.py
@@ -1,20 +1,25 @@
 import os
+import shutil
 from PIL import Image 
 
 img_dir = "./S/"
-res_dir = "../src/assets/S_cropped/"
+res_dir = "../src/assets/S/"
 if not os.path.exists(res_dir):
     os.makedirs(res_dir)
 else:
-    pass
+    print("clearing contents")
+    shutil.rmtree(res_dir)
+    os.makedirs(res_dir)
 for f in os.listdir(img_dir):
     if f.endswith(".png"):
         print("processing {}".format(f))
         src_path = img_dir + f
         img = Image.open(src_path)
+        # 363 x 88
         w, h = img.size
-        left = 100
-        right = w - left
+        # adding offset to make 156x88 so we can scale using common factors
+        left = 100 + 3
+        right = w - left - 1
         top = 0
         bottom = h
         img_res = img.crop((left, top, right, bottom))
