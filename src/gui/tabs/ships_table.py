8b2255789ce2bf720ca025375bf3022d8207f6da
Yanqing Wu <14853314+pwyq@users.noreply.github.com>
2020-10-31 02:06:00 -0400

    read in first 5 columns data

--- src/gui/tabs/ships_table.py
+++ src/gui/tabs/ships_table.py
@@ -8,7 +8,7 @@ from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtCore import Qt
 
 from ...func.helper_function import Helper
-
+from ...func import constants as CONST
 
 def get_data_path(relative_path):
     # This needs to be in current file
@@ -47,23 +47,16 @@ class ShipTable(QTableWidget):
         self.mods = []
 
     def add_ship(self, row, data):
-        self.set_thumbnail(row, data)
-        self.set_name(row, data)
+        self.set_thumbnail(row, str(data["shipCid"]))
+        self.set_name(row, data["title"], data["married"], data["marry_time"])
+        self.set_id(row, data["id"], data["create_time"])
+        self.set_class(row, data["type"])
+        self.set_level(row, data["level"], data["exp"], data["nextExp"])
 
-    def set_name(self, row, data):
-        n = data["title"]
-        n_wig = QTableWidgetItem(n)
-        if data["married"] == 1:
-            n_wig.setIcon(QIcon(get_data_path("src/assets/icons/ring_60.png")))
-            s = "Married on " + self.hlp.ts_to_date(data["marry_time"])
-            n_wig.setToolTip(s)
-        self.setItem(row, 1, n_wig)
-
-    def set_thumbnail(self, row, data):
+    def set_thumbnail(self, row, cid):
+        ''' Column 0
+        Set ship image (thumbnail) and categorize ships by cid along the way.
         '''
-        Set ship image (thumbnail) and categorize cid along the way.
-        '''
-        cid = str(data["shipCid"])
         assert (len(cid) == 8)
 
         if cid[-2:] == "11":
@@ -103,6 +96,34 @@ class ShipTable(QTableWidget):
             self.setItem(row, 0, tmp2)
             err = "Image path does not exist: " + img_path
             logging.warn(err)
+
+    def set_name(self, *args):
+        wig = QTableWidgetItem(args[1])
+        if args[2] == 1:
+            wig.setIcon(QIcon(get_data_path("src/assets/icons/ring_60.png")))
+            s = "Married on " + self.hlp.ts_to_date(args[3])
+            wig.setToolTip(s)
+        self.setItem(args[0], 1, wig)
+
+    def set_id(self, *args):
+        wig = QTableWidgetItem(str(args[1]))
+        s = "Met on " + self.hlp.ts_to_date(args[2])
+        wig.setToolTip(s)
+        self.setItem(args[0], 2, wig)
+
+    def set_class(self, *args):
+        wig = QTableWidgetItem(CONST.ship_type[args[1]])
+        self.setItem(args[0], 3, wig)
+
+    def set_level(self, *args):
+        wig = QTableWidgetItem(str(args[1]))
+        s = "Exp " + str(args[2]) + " / " + str(args[3])
+        wig.setToolTip(s)
+        self.setItem(args[0], 4, wig)
+
+    def set_hp(self, *args):
+        pass
+
 
 
 # End of File
