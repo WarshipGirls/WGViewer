c31ba126034681ab319d433c7d6e40ef2c3d70e4
Yanqing Wu <14853314+pwyq@users.noreply.github.com>
2020-10-31 22:41:58 -0400

    rebuild ship tab

--- src/gui/tabs/ships.py
+++ src/gui/tabs/ships.py
@@ -2,12 +2,15 @@ import sys
 import os
 import logging
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QWidget, QLabel
-from PyQt5.QtWidgets import QVBoxLayout, QGridLayout, QScrollArea
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QVBoxLayout, QGridLayout, QScrollArea, QHBoxLayout
+from PyQt5.QtWidgets import QComboBox, QCheckBox, QTableView
+from PyQt5.QtCore import Qt, pyqtSlot, QSortFilterProxyModel
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
 from ...func import constants as CONST
-from .ships_table import ShipTable
+from .ships_table import ShipTable, ShipTableDelegate
 from .ships_top_checkbox import TopCheckboxes
 
 
@@ -22,64 +25,134 @@ class TabShips(QWidget):
     def __init__(self, realrun):
         super().__init__()
 
-        list_box = QVBoxLayout(self)
-        self.setLayout(list_box)
-
+        scroll_box = QVBoxLayout(self)
+        self.setLayout(scroll_box)
         scroll = QScrollArea(self)
-        list_box.addWidget(scroll)
+        scroll_box.addWidget(scroll)
         scroll.setWidgetResizable(True)
-        scroll_content = QWidget(scroll)
 
-        self.scroll_layout = QVBoxLayout(scroll_content)
-        scroll_content.setLayout(self.scroll_layout)
-        scroll.setWidget(scroll_content)
 
-        # only 20 out of 27 is used by the game at 5.0.0
-        # self.ships = [[]] * 27    # this binds to same list
-        self.ships = []
-        for i in range(27):
-            self.ships.append([])
-        # TODO? https://github.com/WarshipGirls/WGViewer/issues/7
-        self.ship_table = ShipTable()
-        ck = TopCheckboxes(self.ship_table)
+        self.content_widget = QWidget(scroll)
+        self.content_layout = QVBoxLayout(self.content_widget)
+        self.content_widget.setLayout(self.content_layout)
+        scroll.setWidget(self.content_widget)
 
-        self.scroll_layout.addWidget(ck)
-        self.scroll_layout.addWidget(self.ship_table)
+        self.upper_content_widget = QWidget(self.content_widget)
+        self.lower_content_widget = QWidget(self.content_widget)
 
-        self.scroll_layout.setStretch(0, 1)
-        self.scroll_layout.setStretch(1, 10)   # Give space to the table as much as possible
+        self.content_layout.addWidget(self.upper_content_widget)
+        self.content_layout.addWidget(self.lower_content_widget)
+        self.content_layout.setStretch(0, 1)
+        self.content_layout.setStretch(1, 10)
 
-        if realrun == 0:
-            self.test()
-        print("tabships")
-        # self.setFixedHeight(200)
-        print(self.width(), self.height())
+        ck = TopCheckboxes(self.upper_content_widget, 0)
 
-    def test(self):
-        logging.debug("Starting tests")
-        import json
-        p = get_data_path('api_getShipList.json')
-        with open(p) as f:
-            d = json.load(f)
-        self.on_received_shiplist(d)
+        self.table_view = QTableView(self.lower_content_widget)
+        self.lower_layout = QGridLayout(self.lower_content_widget)
+        self.lower_layout.addWidget(self.table_view, 0, 0, 1, 20)
+        self.table_model = QStandardItemModel(self)
 
-    @pyqtSlot(dict)
-    def on_received_shiplist(self, data):
-        if data == None:
-            logging.error("Invalid ship list data.")
-        else:
-            # First sort by level, then sort by cid
-            sorted_ships = sorted(data["userShipVO"], key=lambda x: (x['level'], x['shipCid']), reverse=True)
-            for s in sorted_ships:
-                self.ships[s["type"]].append(s)
+        for rowName in range(15*10):
+            self.table_model.invisibleRootItem().appendRow(
+                [   QStandardItem("row {0} col {1}".format(rowName, column))    
+                    for column in range(20)
+                    ]
+                )
 
-            for ship_type, ship_lists in enumerate(self.ships):
-                if (ship_type not in CONST.ship_type) and (len(ship_lists) != 0):
-                    continue
-                else:
-                    for ship in ship_lists:
-                        self.ship_table.insertRow(self.ship_table.rowCount())
-                        self.ship_table.add_ship(self.ship_table.rowCount()-1, ship)
+        self.table_proxy = QSortFilterProxyModel(self)
+        self.table_proxy.setSourceModel(self.table_model)
+
+        self.table_view.setModel(self.table_proxy)
+        self.table_view.setItemDelegate(ShipTableDelegate(self.table_view))
+        self.table_view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.table_view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        # self.content_widget  = QtWidgets.QWidget(self)
+        # self.lineEdit       = QtWidgets.QLineEdit(self.content_widget)
+        # self.view           = QtWidgets.QTableView(self.content_widget)
+        # self.comboBox       = QtWidgets.QComboBox(self.content_widget)
+        # self.label          = QtWidgets.QLabel(self.content_widget)
+
+        # self.gridLayout = QtWidgets.QGridLayout(self.content_widget)
+        # self.gridLayout.addWidget(self.lineEdit, 0, 1, 1, 1)
+        # self.gridLayout.addWidget(self.view, 1, 0, 1, 3)
+        # self.gridLayout.addWidget(self.comboBox, 0, 2, 1, 1)
+        # self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        # # self.setcontent_widget(self.content_widget)
+        # self.label.setText("Regex Filter")
+        # self.model = QtGui.QStandardItemModel(self)
+        # for rowName in range(3*5):
+        #     self.model.invisibleRootItem().appendRow(
+        #         [   QtGui.QStandardItem("row {0} col {1}".format(rowName, column))    
+        #             for column in range(3)
+        #             ]
+        #         )
+
+        # self.proxy = QtCore.QSortFilterProxyModel(self)
+        # self.proxy.setSourceModel(self.model)
+
+        # self.view.setModel(self.proxy)
+        # self.comboBox.addItems(["Column {0}".format(x) for x in range(self.model.columnCount())])
+
+
+    #     list_box = QVBoxLayout(self)
+    #     self.setLayout(list_box)
+
+    #     scroll = QScrollArea(self)
+    #     list_box.addWidget(scroll)
+    #     scroll.setWidgetResizable(True)
+    #     scroll_content = QWidget(scroll)
+
+    #     self.scroll_layout = QVBoxLayout(scroll_content)
+    #     scroll_content.setLayout(self.scroll_layout)
+    #     scroll.setWidget(scroll_content)
+
+    #     # only 20 out of 27 is used by the game at 5.0.0
+    #     # self.ships = [[]] * 27    # this binds to same list
+    #     self.ships = []
+    #     for i in range(27):
+    #         self.ships.append([])
+    #     # TODO? https://github.com/WarshipGirls/WGViewer/issues/7
+    #     self.ship_table = ShipTable()
+    #     ck = TopCheckboxes(self.ship_table)
+
+    #     self.scroll_layout.addWidget(ck)
+    #     self.scroll_layout.addWidget(self.ship_table)
+
+    #     self.scroll_layout.setStretch(0, 1)
+    #     self.scroll_layout.setStretch(1, 10)   # Give space to the table as much as possible
+
+    #     if realrun == 0:
+    #         self.test()
+    #     print("tabships")
+    #     # self.setFixedHeight(200)
+    #     print(self.width(), self.height())
+
+    # def test(self):
+    #     logging.debug("Starting tests")
+    #     import json
+    #     p = get_data_path('api_getShipList.json')
+    #     with open(p) as f:
+    #         d = json.load(f)
+    #     self.on_received_shiplist(d)
+
+    # @pyqtSlot(dict)
+    # def on_received_shiplist(self, data):
+    #     if data == None:
+    #         logging.error("Invalid ship list data.")
+    #     else:
+    #         # First sort by level, then sort by cid
+    #         sorted_ships = sorted(data["userShipVO"], key=lambda x: (x['level'], x['shipCid']), reverse=True)
+    #         for s in sorted_ships:
+    #             self.ships[s["type"]].append(s)
+
+    #         for ship_type, ship_lists in enumerate(self.ships):
+    #             if (ship_type not in CONST.ship_type) and (len(ship_lists) != 0):
+    #                 continue
+    #             else:
+    #                 for ship in ship_lists:
+    #                     self.ship_table.insertRow(self.ship_table.rowCount())
+    #                     self.ship_table.add_ship(self.ship_table.rowCount()-1, ship)
 
 
 # End of File
--- src/gui/tabs/ships_top_checkbox.py
+++ src/gui/tabs/ships_top_checkbox.py
@@ -4,9 +4,9 @@ from PyQt5.QtWidgets import QHBoxLayout, QGridLayout
 
 
 class TopCheckboxes(QWidget):
-    def __init__(self, table):
+    def __init__(self, parent, table):
         super().__init__()
-        self.layout = QGridLayout(self)
+        self.layout = QGridLayout(parent)
         self.table = table
         # self.layout.setContentsMargins(0,0,0,0)
         # TODO: fix the size
@@ -34,8 +34,8 @@ class TopCheckboxes(QWidget):
         rarity_select = ["\u2606 1", "\u2606 2", "\u2606 3", "\u2606 4", "\u2606 5", "\u2606 6"]
         married_select = ["ALL", "Married Only", "Non Married Only"]
         size_select = ["ALL", "SMALL", "MIDIUM", "LARGE"]
-        # self.add_dropdown("LOCK", lock_select, self.lock_handler, 0, 0)
-        self.add_dropdown("LOCK", lock_select, self.table.on_lock_handler, 0, 0)
+        self.add_dropdown("LOCK", lock_select, self.lock_handler, 0, 0)
+        # self.add_dropdown("LOCK", lock_select, self.table.on_lock_handler, 0, 0)
         self.add_dropdown("LEVEL", level_select, self.level_handler, 0, 1)
         self.add_dropdown("VALUE", value_select, self.value_handler, 0, 2)
         self.add_dropdown("MOD.", mod_select, self.mod_handler, 0, 3)
