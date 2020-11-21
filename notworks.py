b8dd969849e3f0d65ce81971ce76500d7c5e2bce
Yanqing Wu <14853314+pwyq@users.noreply.github.com>
2020-11-21 00:11:50 -0500

    ...

--- notworks.py
+++ notworks.py
@@ -10,32 +10,10 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        # DEBUG: confirmed SCROLL doesn't affect
-        # scroll_box = QVBoxLayout()
-        # scroll = QScrollArea()
-        # scroll_box.addWidget(scroll)
-        # scroll.setWidgetResizable(True)
-
-        # content_widget = QWidget(scroll)
-        # content_widget = QWidget()
-        # content_layout = QVBoxLayout(content_widget)
-        # content_widget.setLayout(content_layout)
-        # scroll.setWidget(content_widget)
-
-        # DEBUG: confirmed up/lo content widgets doesn't affect
-        # lo = QWidget(content_widget)
         lo = QWidget()
-        # content_layout.addWidget(lo)
-
-        # DEBUG: confirmed QGridLayout doesn't affect
-        # lo_layout = QGridLayout(lo)
         lo_layout = QVBoxLayout(lo)
-        # lo_line_1 = QLabel("THIS IS LOW cell 1")
-        # lo_layout.addWidget(lo_line_1, 0, 0, 1, 1)
 
-        # tab = QTableView(lo)
         tab = QTableView()
-        # sti = QStandardItemModel(lo)
         sti = QStandardItemModel()
         sti.appendRow([QStandardItem(str(i)) for i in range(5)])
         sti.insertRow(sti.rowCount())
@@ -48,23 +26,11 @@ class MainWindow(QMainWindow):
         tab.setIndexWidget(sti.index(2, 1), QPushButton("fuck"))
         tab.setModel(sti)
 
-        # lo_layout.addWidget(tab, 1, 0, 1, 6)
         lo_layout.addWidget(tab)
-
-        # widget = QWidget()
-        # widget.setLayout(scroll_box)
-        # self.setCentralWidget(widget)
-        # self.setCentralWidget(widget)
-
-        # self.setCentralWidget(content_widget)
-        
-        # self.setCentralWidget(lo)
-
         self.setCentralWidget(tab)
 
 
 app = QApplication(sys.argv)
-# app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5'))
 window = MainWindow()
 window.resize(1000, 600)
 window.show() # IMPORTANT!!!!! Windows are hidden by default.
--- works.py
+++ works.py
@@ -6,22 +6,12 @@ from PyQt5.QtGui import *
 
 
 class EquipsArray(QWidget):
-    # def __init__(self, parent, equips_array):
     def __init__(self):
         super().__init__()
-
-        try:
-            # add your buttons
-            layout = QHBoxLayout(self)
-            print("FUCK MFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
-            layout.addWidget(QPushButton('fuck'))
-            layout.addWidget(QPushButton('fuck'))
-            # layout.addWidget(QPushButton('fuck'))
-            # layout.addWidget(QPushButton('fuck'))
-            self.setLayout(layout)
-        except Exception as e:
-            logging.error(traceback.format_exc())
-            # Logs the error appropriately. 
+        layout = QHBoxLayout(self)
+        layout.addWidget(QPushButton('btn1'))
+        layout.addWidget(QPushButton('btn2'))
+        self.setLayout(layout)
 
 
 if __name__ == '__main__':
@@ -30,28 +20,25 @@ if __name__ == '__main__':
 
     tab = QTableView()
     sti = QStandardItemModel()
+    tab.setModel(sti)
     sti.appendRow([QStandardItem(str(i)) for i in range(5)])
     sti.appendRow([QStandardItem(str(i)) for i in range(3)])    # preset QStandardItem or not doesn't affect
     sti.appendRow([QStandardItem(str(i)) for i in range(4)])
     sti.appendRow([QStandardItem(str(i)) for i in range(4)])
-    # sti.appendRow([QStandardItem(str(i)) for i in range(4)])
     sti.appendRow([])   # empty appendrow doesn't affect
     sti.insertRow(sti.rowCount())   # insertrow also works
     sti.appendRow([QStandardItem(str(i)) for i in range(4)])
-    tab.setModel(sti)
-    # tab.setEditTriggers(QAbstractItemView.NoEditTriggers)
     # tab.setIndexWidget(sti.index(0, 3), QPushButton("button"))
     tab.setIndexWidget(sti.index(0, 3), EquipsArray())
     tab.setIndexWidget(sti.index(1, 3), EquipsArray())
     tab.setIndexWidget(sti.index(2, 3), EquipsArray())
     tab.setIndexWidget(sti.index(3, 4), EquipsArray())
     tab.setIndexWidget(sti.index(4, 3), EquipsArray())
+    sti.setItem(1, 0, QStandardItem("???"))
+    sti.setItem(1, 2, QStandardItem("???"))
 
     idx = sti.index(5, 2)
-    print(idx)
     tab.setIndexWidget(idx, EquipsArray())
-
-    sti.setItem(6, 2, QStandardItem("WHYYYYY"))
 
     tab.show()
     tab.resize(800, 600)
