18c5d03af92e2f756d2a1bb4d4527ea6c9e9acf6
Yanqing Wu <14853314+pwyq@users.noreply.github.com>
2020-11-23 22:02:16 -0500

    done most init data dumping

--- tools/getInitConfigs.py
+++ tools/getInitConfigs.py
@@ -1,55 +1,98 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-import pprint
-import json
-import tempfile
-
-
-# url = 'http://login.jr.moefantasy.com/index/getInitConfigs'
-
-tmp_path = tempfile.gettempdir()
 import os
-# print os.getenv('APPDATA')
+import json
+import shutil
+import logging
+import platform
+import urllib.request
 
-def get_data():
+from pathlib import Path
+
+
+def get_storage_dir():
+    # TODO: unix not tested
+    # TODO: mac not tested
+    plt = platform.system()
+    _dir = ''
+    if plt == "Windows":
+        _dir = os.getenv('LOCALAPPDATA')
+    elif plt == "Linux":
+        _dir = os.path.join(str(Path.home()), '.config')
+    elif plt == "Darwin":
+        _dir = os.path.join(str(Path.home()), 'Library', 'Application Support')
+    else:
+        _dir = str(Path.home())
+    return os.path.join(_dir, 'WarshipGirlsViewer')
+
+def save_data_by_attr(storage_dir, data_dict, field):
+    filename = field + '.json'
+    p = os.path.join(storage_dir, filename)
+    if os.path.exists(p):
+        pass
+    else:
+        with open(p, 'w', encoding='utf-8') as fout:
+            json.dump(data_dict[field], fout, ensure_ascii=False, indent=4)
+
+def _clear_cache():
+    _dir = get_storage_dir()
+    for filename in os.listdir(_dir):
+        file_path = os.path.join(_dir, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            logging.error('Failed to delete %s. Reason: %s' % (file_path, e))
+
+def check_data_ver(storage_dir):
+    # Note: since getting raw data takes 20+ seconds, use this method minimally
+    url = 'http://login.jr.moefantasy.com/index/getInitConfigs'
+    try:
+        # with urllib.request.urlopen(url) as f:
+        #  print(f.read())
+        #  # d = json.loads(f.read().decode('utf-8'))
+        #  d = json.loads(f.read())
+        import requests
+        d = requests.get(url).json()
+    except (urllib.error.URLError, json.decoder.JSONDecodeError) as e:
+        logging.error('Server connection error. Please try again later.')
+        logging.error(e)
+
+    path = os.path.join(storage_dir, 'DataVersion.json')
+    if not os.path.exists(path):
+        res = [False, d]
+        logging.info('DataVersion file is missing. Updating now.')
+    else:
+        with open(path, encoding='utf-8') as f:
+            x = json.load(f)
+            if x == d['DataVersion']:
+                res = [True, {}]
+                logging.info('getInitConfigs data is already up-to-date.')
+            else:
+                res = [False, d]
+                logging.info('getInitConfigs data is updating.')
+    return res
+
+def save_init_data():
+    '''
+    Updating data to the latest version
+    '''
     # TODO: following need to be executed once for new user.
-    # Save chinese (instead of stupid unicode) into output file.
-    with open('../getinitConfigs.json') as f:
-        d = json.load(f)
-        # Uncomment below to see output in terminal
-        # pprint.pprint(d)
+    storage_dir = os.path.join(get_storage_dir(), 'init')
+    if not os.path.exists(storage_dir):
+        os.makedirs(storage_dir)
+    else:
+        pass
 
-        # with open('full_getinitConfigs.txt', 'w', encoding='utf-8') as fout:
-        #     json.dump(d, fout, ensure_ascii=False, indent=4)
-
-        # with open('shipCard.json', 'w', encoding='utf-8') as fout:
-            # json.dump(d['shipCard'], fout, ensure_ascii=False, indent=4)
-        # print(d)
-        f_ship = tempfile.TemporaryFile()
-        # f_ship.write(d['shipCard'])
-        f_ship.write(b'bitch')
-
-        # with open('shipTactics.json', 'w', encoding='utf-8') as fout:
-        #     json.dump(d['ShipTactics'], fout, ensure_ascii=False, indent=4)
-
-        # with open('pveExplore.json', 'w', encoding='utf-8') as fout:
-        #     json.dump(d['pveExplore'], fout, ensure_ascii=False, indent=4)
-
-        # with open('shipEquipmnt.json', 'w', encoding='utf-8') as fout:
-        #     json.dump(d['shipEquipmnt'], fout, ensure_ascii=False, indent=4)
-
-        # with open('shipItem.json', 'w', encoding='utf-8') as fout:
-        #     json.dump(d['shipItem'], fout, ensure_ascii=False, indent=4)
-
-        # with open('shipCampaignLevel.json', 'w', encoding='utf-8') as fout:
-        #     json.dump(d['shipCampaignLevel'], fout, ensure_ascii=False, indent=4)
-
-        # with open('shipSkil1.json', 'w', encoding='utf-8') as fout:
-        #     json.dump(d['shipSkil1'], fout, ensure_ascii=False, indent=4)
-
-        # with open('errorCode.json', 'w', encoding='utf-8') as fout:
-        #     json.dump(d['errorCode'], fout, ensure_ascii=False, indent=4)
-
+    res = check_data_ver(storage_dir)
+    if res[0] == True:
+        pass
+    else:
+        data = res[1]
+        for k in data.keys():
+            save_data_by_attr(storage_dir, data, k)        
 
 def map_equip(equipable_type):
     with open('shipEquipmnt.json', encoding='utf-8') as f1:
@@ -93,4 +136,5 @@ def type_equip_map(cid):
 # fff = type_equip_map(10000213)
 # map_equip(fff)
 
-get_data()
+_clear_cache()
+save_init_data()
