f11ff100ad81532c446e9805a5385908b72bc4c8
Yanqing Wu <14853314+pwyq@users.noreply.github.com>
2020-11-28 18:47:24 -0500

    DATA - reduce code

--- src/data/json.py
+++ src/data/json.py
@@ -7,81 +7,53 @@ from .path import get_init_dir, get_user_dir, get_temp_dir
 
 """
 Order functions alphabetically!
-TODO: reduce code!
 """
 
 def _load_json(folder, filename):
     path = os.path.join(folder, filename)
-    with open(path, encoding='utf-8') as f:
-        t = json.load(f)
+    with open(path, encoding='utf-8') as fin:
+        t = json.load(fin)
     return t
+
+def _save_json(folder, filename, data):
+    path = os.path.join(folder, filename)
+    with open(path, 'w', encoding='utf-8') as fout:
+        json.dump(data, fout, ensure_ascii=False, indent=4)
 
 def get_tactics_json():
-    # return _load_json(get_init_dir(), 'ShipTactics.json')
-    path = os.path.join(get_init_dir(), 'ShipTactics.json')
-    with open(path, encoding='utf-8') as f:
-        t = json.load(f)
-    return t
+    return _load_json(get_init_dir(), 'ShipTactics.json')
 
 def get_user_fleets():
-    path = os.path.join(get_user_dir(), 'fleetVo.json')
-    with open(path, encoding='utf-8') as f:
-        t = json.load(f)
-    return t
+    return _load_json(get_user_dir(), 'fleetVo.json')
 
 def get_user_tactics():
-    path = os.path.join(get_user_dir(), 'tactics.json')
-    with open(path, encoding='utf-8') as f:
-        t = json.load(f)
-    return t
+    return _load_json(get_user_dir(), 'tactics.json')
 
 def get_processed_userShipVo():
-    path = os.path.join(get_user_dir(), 'proc_userShipVo.json')
-    with open(path, encoding='utf-8') as f:
-        t = json.load(f)
-    return t
+    return _load_json(get_user_dir(), 'proc_userShipVo.json')
 
 def get_shipItem():
-    path = os.path.join(get_init_dir(), 'shipItem.json')
-    with open(path, encoding='utf-8') as f:
-        t = json.load(f)
-    return t
+    return _load_json(get_init_dir(), 'shipItem.json')
 
 def get_userVo():
-    path = os.path.join(get_user_dir(), 'userVo.json')
-    with open(path, encoding='utf-8') as f:
-        t = json.load(f)
-    return t
+    return _load_json(get_user_dir(), 'userVo.json')
 
 def get_equipmentVo():
     # shipEquipmnt.json contains the equipment you own;
     # the `num` excludes those on ship
-    user_equip_path = os.path.join(get_user_dir(), 'equipmentVo.json')
-    with open(user_equip_path, encoding='utf-8') as f2:
-        user_equips = json.load(f2)
-    return user_equips
+    return _load_json(get_user_dir(), 'equipmentVo.json')
 
 def save_equipmentVo(user_equips):
-    user_equip_path = os.path.join(get_user_dir(), 'equipmentVo.json')
-    with open(user_equip_path, 'w', encoding='utf-8') as fout:
-        json.dump(user_equips, fout, ensure_ascii=False, indent=4)
+    _save_json(get_user_dir(), 'equipmentVo.json', user_equips)
 
 def get_shipCard():
-    p = os.path.join(get_init_dir(), 'shipCard.json')
-    with open(p, encoding='utf-8') as f:
-        x = json.load(f)
-    return x
+    return _load_json(get_init_dir(), 'shipCard.json')
 
 def get_shipEquipmnt(): # No typo
-    equip_path = os.path.join(get_init_dir(), 'shipEquipmnt.json')
-    with open(equip_path, encoding='utf-8') as f1:
-        all_equips = json.load(f1)
-    return all_equips
+    return _load_json(get_init_dir(), 'shipEquipmnt.json')
 
 def save_processed_userShipVo(data):
-    path = os.path.join(get_user_dir(), 'proc_userShipVo.json')
-    with open(path, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+    _save_json(get_user_dir(), 'proc_userShipVo.json', data)
 
 def init_ships_temp():
     data = {}
@@ -90,9 +62,7 @@ def init_ships_temp():
         with open(path, encoding='utf-8') as f:
             data = json.load(f)
     else:
-        card_path = os.path.join(get_init_dir(), 'shipCard.json')
-        with open(card_path, encoding='utf-8') as f:
-            cards = json.load(f)
+        cards = get_shipCard()
         for c in cards:
             data[c['cid']] = {}
             data[c['cid']]['rarity'] = c['star']
@@ -101,3 +71,6 @@ def init_ships_temp():
             json.dump(data, fout, ensure_ascii=False, indent=4)
 
     return data
+
+
+# End of File
