637f809bbf2c0274fbb8861d35a3bab2efd10dac
pwyq <y433wu@edu.uwaterloo.ca>
2020-12-10 23:01:29 -0500

    Add bypass login option #102

--- src/data/wgv_qsettings.py
+++ src/data/wgv_qsettings.py
@@ -1,7 +1,20 @@
 import os
+import pickle
+
 import qdarkstyle
 
+from ast import literal_eval
+
 from PyQt5.QtCore import QSettings
+
+from src.func.encryptor import Encryptor
+
+
+def del_key_file(key_file: str = '.wgr.key') -> None:
+    if is_key_exists(key_file):
+        os.remove(os.path.join(get_data_dir(), key_file))
+    else:
+        pass
 
 
 def get_color_scheme() -> str:
@@ -18,19 +31,37 @@ def get_qsettings_file() -> str:
     return os.path.join(get_data_dir(), 'wgviewer.ini')
 
 
-def get_key_path(key_file: str) -> str:
+def get_key_path(key_file: str = '.wgr.key') -> str:
     return os.path.join(get_data_dir(), key_file)
 
 
-def is_key_exists(key_file: str) -> bool:
+def is_key_exists(key_file: str = '.wgr.key') -> bool:
     return os.path.exists(os.path.join(get_data_dir(), key_file))
 
 
-def del_key_file(key_file: str) -> None:
-    if is_key_exists(key_file):
-        os.remove(os.path.join(get_data_dir(), key_file))
+def save_cookies(cookie: dict) -> None:
+    encryptor = Encryptor()
+    key = encryptor.load_key(get_key_path())
+    data = encryptor.encrypt_str(key, str(cookie))
+    with open(os.path.join(get_data_dir(), 'user.cookies'), 'wb') as f:
+        pickle.dump(data, f)
+
+
+def load_cookies() -> dict:
+    cookie_path = os.path.join(get_data_dir(), 'user.cookies')
+    with open(cookie_path, 'rb') as f:
+        encrypted_cookie = pickle.load(f)
+    encryptor = Encryptor()
+    if is_key_exists():
+        try:
+            key = encryptor.load_key(get_key_path())
+            string = encryptor.decrypt_data(key, encrypted_cookie).decode('utf-8')
+            res = literal_eval(string)
+        except (AssertionError, AttributeError):
+            res = {}
     else:
-        pass
+        res = {}
+    return res
 
 
 if __name__ == "__main__":
