1047735efc8f5939f7fae3d80feb85d58dc80a98
pwyq <y433wu@edu.uwaterloo.ca>
2020-12-13 02:56:45 -0500

    [MENU] clean up

--- src/gui/interface/settings.py
+++ src/gui/interface/settings.py
@@ -1,24 +1,35 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QPoint, QRect
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (
+    QStyle, QStyleOptionTab, QStylePainter,
+    QTabBar, QTabWidget,
+    QProxyStyle, QMainWindow, QWidget
+)
+
+from src.utils.wgv_pyqt import get_user_resolution
 
 
-class TabBar(QtWidgets.QTabBar):
+class TabBar(QTabBar):
+    def __init__(self):
+        super().__init__()
+
     def tabSizeHint(self, index):
-        s = QtWidgets.QTabBar.tabSizeHint(self, index)
+        s = QTabBar.tabSizeHint(self, index)
         s.transpose()
         return s
 
     def paintEvent(self, event):
-        painter = QtWidgets.QStylePainter(self)
-        opt = QtWidgets.QStyleOptionTab()
+        painter = QStylePainter(self)
+        opt = QStyleOptionTab()
 
         for i in range(self.count()):
             self.initStyleOption(opt, i)
-            painter.drawControl(QtWidgets.QStyle.CE_TabBarTabShape, opt)
+            painter.drawControl(QStyle.CE_TabBarTabShape, opt)
             painter.save()
 
             s = opt.rect.size()
             s.transpose()
-            r = QtCore.QRect(QtCore.QPoint(), s)
+            r = QRect(QPoint(), s)
             r.moveCenter(opt.rect.center())
             opt.rect = r
 
@@ -26,41 +37,42 @@ class TabBar(QtWidgets.QTabBar):
             painter.translate(c)
             painter.rotate(90)
             painter.translate(-c)
-            painter.drawControl(QtWidgets.QStyle.CE_TabBarTabLabel, opt)
+            painter.drawControl(QStyle.CE_TabBarTabLabel, opt)
             painter.restore()
 
 
-class TabWidget(QtWidgets.QTabWidget):
-    def __init__(self, *args, **kwargs):
-        QtWidgets.QTabWidget.__init__(self, *args, **kwargs)
-        self.setTabBar(TabBar(self))
-        self.setTabPosition(QtWidgets.QTabWidget.West)
+class TabWidget(QTabWidget):
+    def __init__(self):
+        super().__init__()
+        self.setTabBar(TabBar())
+        self.setTabPosition(QTabWidget.West)
 
 
-class ProxyStyle(QtWidgets.QProxyStyle):
+class ProxyStyle(QProxyStyle):
+    def __init__(self):
+        super().__init__()
+
     def drawControl(self, element, opt, painter, widget):
-        if element == QtWidgets.QStyle.CE_TabBarTabLabel:
-            ic = self.pixelMetric(QtWidgets.QStyle.PM_TabBarIconSize)
-            r = QtCore.QRect(opt.rect)
-            w = 0 if opt.icon.isNull() else opt.rect.width() + self.pixelMetric(QtWidgets.QStyle.PM_TabBarIconSize)
+        if element == QStyle.CE_TabBarTabLabel:
+            r = QRect(opt.rect)
+            w = 0 if opt.icon.isNull() else opt.rect.width() + self.pixelMetric(QStyle.PM_TabBarIconSize)
             r.setHeight(opt.fontMetrics.width(opt.text) + w)
             r.moveBottom(opt.rect.bottom())
             opt.rect = r
-        QtWidgets.QProxyStyle.drawControl(self, element, opt, painter, widget)
+        QProxyStyle.drawControl(self, element, opt, painter, widget)
 
 
-class GlobalSettingsWindow(QtWidgets.QMainWindow):
-
+class GlobalSettingsWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setStyle(ProxyStyle())
         self.vertical_tabs = TabWidget()
-        self.vertical_tabs.addTab(QtWidgets.QWidget(), QtGui.QIcon("zoom.png"), "ABC")
-        self.vertical_tabs.addTab(QtWidgets.QWidget(), QtGui.QIcon("zoom.png"), "ABC")
-        self.vertical_tabs.addTab(QtWidgets.QWidget(), QtGui.QIcon("zoom.png"), "ABC")
-        self.vertical_tabs.addTab(QtWidgets.QWidget(), QtGui.QIcon("zoom.png"), "ABC")
-        self.vertical_tabs.resize(640, 480)
+        self.vertical_tabs.addTab(QWidget(), QIcon("assets/favicon.ico"), "ABC")
+        # self.vertical_tabs.addTab(, QIcon("assets/favicon.ico"), "ABC")
+        user_w, user_h = get_user_resolution()
+        w = int(user_w / 3)
+        h = int(user_h * 4 / 9)
+        self.vertical_tabs.resize(w, h)
         self.vertical_tabs.show()
-
 
 # End of File
